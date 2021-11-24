9aad41b074949efb53fe1bdc5ac20f68d081638c
fishball <hychanbn1009@gmail.com>
2021-11-25 00:22:22 +0800

    cv-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+  render(){
+    return (
+      <div>
+        <form>
+          <div className='personalInfo'>
+            <h3>Personal Information</h3>
+            <p>Name</p>
+            <input type='text' id='name' placeholder='Name'/>
+            <p>Email</p>
+            <input type='email' id='email' placeholder='Email'/>
+            <p>Phone</p>
+            <input type='tel' id='phone' placeholder='Phone'/>
+          </div>
+          <div className='educationalExperience'>
+            <h3>Educational Experience</h3>
+            <p>School Name</p>
+            <input type='text' id='schoolName' placeholder='School Name'/>
+            <p>Title of study</p>
+            <input type='text' id='schoolTitle' placeholder='School Title'/>
+            <p>Date of Study</p>
+            <input type='date' id='studyDate' placeholder='Study Date'/>
+          </div>
+          <div className='practicalExperience'>
+            <h3>Practical Experience</h3>
+            <p>Company Name</p>
+            <input type='text' id='companyName' placeholder='Company Name'/>
+            <p>Position Title</p>
+            <input type='text' id='positionTitle' placeholder='Position Title'/>
+            <p>Job Duty</p>
+            <input type='date' id='duty' placeholder='Job Duty'/>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
